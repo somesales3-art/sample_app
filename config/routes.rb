bbe27e794131f906be36805ed9c92a22d16469d8
some <somesales3@gmail.com>
2025-12-30 14:49:21 +0900

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   root "staticpage#home"
   get "/help",    to: "staticpage#help"
   get "/about",   to: "staticpage#about"
   get "/contact", to: "staticpage#contact"
   get "/signup",  to: "users#new" 
+  get    "/login",   to: "sessions#new"
+  post   "/login",   to: "sessions#create"
+  delete "/logout",  to: "sessions#destroy"
   resources :users
 
 end
