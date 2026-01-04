d48436d74c4ce2ada35cff8ee9f399ed75118f85
some <somesales3@gmail.com>
2026-01-04 19:24:47 +0900

    Add password reset

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -6,8 +6,8 @@ class UserMailerPreview < ActionMailer::Preview
   end
 
   def password_reset
-    user = User.first || User.new(name: "Preview User", email: "preview@example.com")
-    user.reset_token = User.new_token if user.respond_to?(:reset_token=)
+    user = User.first
+    user.reset_token = User.new_token
     UserMailer.password_reset(user)
   end
 end
