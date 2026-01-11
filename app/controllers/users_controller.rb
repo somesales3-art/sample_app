628cd6af66e30ee741ac10b4df60f44e72f49936
somesales3-art <somesales3@gmail.com>
2026-01-11 09:19:59 +0000

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -52,14 +53,6 @@ class UsersController < ApplicationController
                                     :password_confirmation)
     end
     
-      # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
-      end
-    end
     
     def correct_user
       @user = User.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,29 +1,37 @@
 class User < ApplicationRecord
+  # dependent: :destroy をつけたこの1行だけでOKです
+  has_many :microposts, dependent: :destroy
+
+  # 重複していた定義を1行にまとめました
   attr_accessor :remember_token, :activation_token, :reset_token
-  attr_accessor :remember_token, :activation_token
+
   before_save   :downcase_email
   before_create :create_activation_digest
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
-  validates :name, presence:true, length:{maximum:50}
+
+  validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence:true, length:{maximum:255},
+  validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 },allow_nil: true
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-  def User.digest(string)
-      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                    BCrypt::Engine.cost
-      BCrypt::Password.create(string, cost: cost)
+  # 渡された文字列のハッシュ値を返す
+  def self.digest(string)
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create(string, cost: cost)
   end
 
-  def User.new_token
+  # ランダムなトークンを返す
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
 
-    # 永続的セッションのためにユーザーをデータベースに記憶する
+  # 永続的セッションのためにユーザーをデータベースに記憶する
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
@@ -35,11 +43,12 @@ class User < ApplicationRecord
   def session_token
     remember_digest || remember
   end
-  
+
   # 渡されたトークンがダイジェストと一致したらtrueを返す
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return false if digest.nil?
+
     BCrypt::Password.new(digest).is_password?(token)
   end
 
@@ -70,23 +79,26 @@ class User < ApplicationRecord
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
-  
+
   # パスワード再設定の期限が切れている場合はtrueを返す
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
 
+  def feed
+    Micropost.where("user_id=?",id)
+  end
+
   private
 
-    # メールアドレスをすべて小文字にする
-    def downcase_email
-      self.email = email.downcase
-    end
+  # メールアドレスをすべて小文字にする
+  def downcase_email
+    self.email = email.downcase
+  end
 
-    # 有効化トークンとダイジェストを作成および代入する
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
-
+  # 有効化トークンとダイジェストを作成および代入する
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
